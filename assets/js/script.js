834386c880c3fd129c56bfc73276275d585e0c1f
parides55 <>
2023-12-03 21:02:06 +0000

    add event listeners

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,28 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        });
+    }
+});
+
 function runGamme() {
 
 }
 
 function checkAnswer() {
-    
+
 }
 
 function calculateCorrectAnswer() {
-    
+
 }
 
 function incrementScore() {
@@ -23,10 +38,10 @@ function displayAdditionQuestion() {
 }
 
 function displaySubtractQuestion() {
-    
+
 }
 
 function displayMultiplyQuestion() {
-    
+
 }
 
